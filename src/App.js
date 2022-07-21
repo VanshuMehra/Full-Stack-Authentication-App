3b765ced206ca6a9beb73076c7151c9b50b11bce
VanshuMehra <vanshumehra397@gmail.com>
2022-07-21 16:29:51 +0530

    Created login component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { Container, Col, Row } from "react-bootstrap";
 import "./App.css";
 import Register from "./Register";
+import Login from "./Login";
 
 function App() {
   return (
     <Container>
       <Row>
-        <Col xs={12} sm={12} md={6} lg={6}></Col>
-        <Register />
-        <Col xs={12} sm={12} md={6} lg={6}></Col>
+        <Col xs={12} sm={12} md={6} lg={6}>
+          <Register />
+        </Col>
+
+        <Col xs={12} sm={12} md={6} lg={6}>
+          <Login />
+        </Col>
       </Row>
     </Container>
   );
